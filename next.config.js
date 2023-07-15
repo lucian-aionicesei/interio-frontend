b6de9c9cc89cd3cbd4579c8902cbdf9b545299d2
Lucian <la@checkmate.dk>
2023-07-15 21:02:06 +0200

    next/image is configures in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+if (!process.env.WORDPRESS_API_URL) {
+  throw new Error(`
+      Please provide a valid WordPress instance URL.
+      Add to your environment variables WORDPRESS_API_URL.
+    `);
+}
 
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains: [
+      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      "interio-db.graphite.dk",
+    ],
+  },
+};
