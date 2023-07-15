70277e3dc4f29b8752c1640ffce2861456d653b4
Lucian <la@checkmate.dk>
2023-07-15 20:55:33 +0200

    changes next.config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-if (!process.env.WORDPRESS_API_URL) {
-  throw new Error(`
-      Please provide a valid WordPress instance URL.
-      Add to your environment variables WORDPRESS_API_URL.
-    `);
-}
-
 /** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    domains: [
-      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
-      "interio-db.graphite.dk",
-      "1.gravatar.com",
-      "2.gravatar.com",
-      "secure.gravatar.com",
-      "interio-db.graphite.dk",
-    ],
-  },
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
